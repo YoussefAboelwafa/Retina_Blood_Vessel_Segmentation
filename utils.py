13bcb337e06379193d0c0a27b0409183147e66d4
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-15 18:06:01 +0300

    utils

--- utils.py
+++ utils.py
@@ -20,4 +20,10 @@ def load_data(directory_path):
     train_masks_path = directory_path + "/train/mask"
     test_images_path = directory_path + "/test/image"
     test_masks_path = directory_path + "/test/mask"
-    return train_images_path, train_masks_path, test_images_path, test_masks_path
+
+    tain_images = load_images_from_directory(train_images_path)
+    train_masks = load_images_from_directory(train_masks_path)
+    test_images = load_images_from_directory(test_images_path)
+    test_masks = load_images_from_directory(test_masks_path)
+
+    return tain_images, train_masks, test_images, test_masks
