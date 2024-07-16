0010b11dee1eee8146da7b363a304280aba36341
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-16 14:32:28 +0300

    dataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms.functional as TF
+from torch.utils.data import Dataset
+import cv2 as cv
 
 
 class RetinaDataset(Dataset):
@@ -14,11 +14,21 @@ class RetinaDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.images[idx]
-        mask = self.masks[idx]
+        image = cv.imread(self.images[idx], cv.IMREAD_COLOR)
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        image = image / 255.0
+        image = image.astype(np.float32)
+        image = np.transpose(image, (2, 0, 1))
+        image_tensor = torch.from_numpy(image)
+
+        mask = cv.imread(self.masks[idx], cv.IMREAD_GRAYSCALE)
+        mask = mask / 255.0
+        mask = mask.astype(np.float32)
+        mask_tensor = torch.from_numpy(mask)
+        mask_tensor = mask_tensor.unsqueeze(0)
 
         if self.transform:
-            image = self.transform(image)
-            mask = self.transform(mask)
+            image_tensor = self.transform(image_tensor)
+            mask_tensor = self.transform(mask_tensor)
 
-        return image, mask
+        return image_tensor, mask_tensor
