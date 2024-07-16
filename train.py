6c56bc2f3eb57d485d934885d83fd6f5df4998c0
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-16 13:03:01 +0300

    dataset

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 import torch
 from model import UNet
 from dataset import RetinaDataset
-from utils import *
+from utils import load_data
+import warnings
+warnings.filterwarnings("ignore")
 
-model = UNet(3, 1)
-input_image = torch.rand(1, 3, 512, 512)
-output = model(input_image)
-print(type(output), output.size())
--- utils.py
+++ utils.py
@@ -1,27 +1,10 @@
 import os
-from PIL import Image
-
-
-def load_images_from_directory(directory_path):
-    images = []
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".png"):
-            img_path = os.path.join(directory_path, filename)
-            try:
-                with Image.open(img_path) as img:
-                    images.append(img.copy())
-            except Exception as e:
-                print(f"Failed to load image {filename}: {e}")
-    return images
+from glob import glob
 
 
 def load_data(base_directory="dataset"):
-    train_images_path = base_directory + "/train/image"
-    train_masks_path = base_directory + "/train/mask"
-    test_images_path = base_directory + "/test/image"
-    test_masks_path = base_directory + "/test/mask"
-    tain_images = load_images_from_directory(train_images_path)
-    train_masks = load_images_from_directory(train_masks_path)
-    test_images = load_images_from_directory(test_images_path)
-    test_masks = load_images_from_directory(test_masks_path)
-    return tain_images, train_masks, test_images, test_masks
+    train_images = sorted(glob(os.path.join(base_directory, "train", "image", "*.png")))
+    train_masks = sorted(glob(os.path.join(base_directory, "train", "mask", "*.png")))
+    test_images = sorted(glob(os.path.join(base_directory, "test", "image", "*.png")))
+    test_masks = sorted(glob(os.path.join(base_directory, "test", "mask", "*.png")))
+    return train_images, train_masks, test_images, test_masks
