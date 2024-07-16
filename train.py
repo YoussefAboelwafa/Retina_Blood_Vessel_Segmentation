28b6568650fe71f8a82fc2761d942f54816b4c28
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-16 19:35:22 +0300

    train

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from dataset import RetinaDataset
 from torch.utils.data import DataLoader
 from utils import load_data
 import warnings
+import segmentation_models_pytorch as smp
 import json
 
 warnings.filterwarnings("ignore")
@@ -13,8 +14,12 @@ EPOCHS = 50
 LR = 1e-4
 IN_CHANNELS = 3
 OUT_CHANNELS = 1
-CHECKPOINT_PATH = "/scratch/y.aboelwafa/Retina_Blood_Vessel_Segmentation/checkpoints/checkpoint.pth"
-METRICS_PATH = "/scratch/y.aboelwafa/Retina_Blood_Vessel_Segmentation/metrics/metrics.json"
+CHECKPOINT_PATH = (
+    "/scratch/y.aboelwafa/Retina_Blood_Vessel_Segmentation/checkpoints/checkpoint.pth"
+)
+METRICS_PATH = (
+    "/scratch/y.aboelwafa/Retina_Blood_Vessel_Segmentation/metrics/metrics.json"
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -26,3 +31,37 @@ test_dataset = RetinaDataset(test_images, test_masks)
 train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
+model = UNet(in_channels=IN_CHANNELS, out_channels=OUT_CHANNELS).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+criterion = smp.losses.DiceLoss(mode="binary")
+
+metrics = {
+    "train_loss": [],
+    "val_loss": [],
+    "train_iou_score": [],
+    "val_iou_score": [],
+    "train_dice_score": [],
+    "val_dice_score": [],
+}
+
+for epoch in range(EPOCHS):
+    model.train()
+    for batch_idx, (image, mask) in enumerate(train_dataloader):
+        image = image.to(device=device)
+        mask = mask.to(device=device)
+        optimizer.zero_grad()
+        pred = model(image)
+        loss = criterion(pred, mask)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 5 == 0:
+            print(f"Epoch {epoch}, batch {batch_idx}, loss: {loss.item()}")
+
+    model.eval()
+    with torch.no_grad():
+        for image, mask in test_dataloader:
+            image = image.to(device=device)
+            mask = mask.float().to(device=device)
+            pred = model(image)
+            loss = criterion(pred, mask)
+            print(f"Validation loss: {loss.item()}")
