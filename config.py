83948f3aef2da0436b6a508349daad6e31193891
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-22 19:59:44 +0300

    config file

---
+++ config.py
@@ -0,0 +1,28 @@
+import argparse
+import warnings
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int, default=200)
+parser.add_argument("--lr", type=float, default=0.0001)
+parser.add_argument("--batch_size", type=int, default=4)
+parser.add_argument("--gpus", type=int, default=1)
+parser.add_argument("--job_id", type=int)
+args = parser.parse_args()
+
+
+EPOCHS = args.epochs
+LR = args.lr
+BATCH_SIZE = args.batch_size
+GPUS = args.gpus
+
+IN_CHANNELS = 3
+OUT_CHANNELS = 1
+
+BASE_DIRECTORY = "dataset"
+CHECKPOINT_PATH = "/scratch/y.aboelwafa/Retina/Retina_Blood_Vessel_Segmentation/checkpoints/checkpoint"
+
+ACCELERATOR = "gpu"
+
+
+
