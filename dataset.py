6c1e3a3a5548609d47efc2c278b4038f33a772be
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-23 15:05:57 +0300

    Z-Normalization

--- dataset.py
+++ dataset.py
@@ -1,8 +1,8 @@
 import numpy as np
-import torch
 from torch.utils.data import Dataset
 import cv2 as cv
 from torchvision.transforms import ToTensor
+from torchvision.transforms import Normalize
 
 
 class RetinaDataset(Dataset):
@@ -10,6 +10,7 @@ class RetinaDataset(Dataset):
         self.images = images
         self.masks = masks
         self.to_tensor = ToTensor()
+        self.normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.transform = transform
 
     def __len__(self):
@@ -18,18 +19,23 @@ class RetinaDataset(Dataset):
     def __getitem__(self, idx):
         image = cv.imread(self.images[idx], cv.IMREAD_COLOR)
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        image = image / 255.0
+        mean = image.mean(axis=(0, 1))
+        std = image.std(axis=(0, 1))
+        image = (image - mean[None, None, :]) / std[None, None, :]
+        # image = image / 255.0
         image = image.astype(np.float32)
 
         mask = cv.imread(self.masks[idx], cv.IMREAD_GRAYSCALE)
         mask[mask > 0] = 1
         mask = mask.astype(np.float32)
 
-        image_tensor = self.to_tensor(image)
-        mask_tensor = self.to_tensor(mask)
+        
 
         if self.transform:
-            augmented = self.transform(image=image_tensor, mask=mask_tensor)
-            image_tensor, mask_tensor = augmented["image"], augmented["mask"]
-
+            augmented = self.transform(image=image, mask=mask)
+            image, mask = augmented["image"], augmented["mask"]
+        
+        image_tensor = self.to_tensor(image)
+        mask_tensor = self.to_tensor(mask)
+        
         return image_tensor, mask_tensor
