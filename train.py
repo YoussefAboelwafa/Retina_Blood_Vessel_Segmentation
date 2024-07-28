8369dd013dc3ea4837ac32ec16130d8a556c4675
YoussefAboelwafa <youssef.hossam49@gmail.com>
2024-07-28 19:04:13 +0300

    callbacks

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 from pytorch_lightning.loggers import CometLogger
 from utils import *
 from config import *
+from callbacks import *
 from model import LitUnet
 from dataset import RetinaDataModule
 import pytorch_lightning as pl
 import albumentations as A
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 set_seed()
 
@@ -27,44 +27,6 @@ train_transform = A.Compose(
     ]
 )
 
-
-class CustomModelCheckpoint(ModelCheckpoint):
-    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
-        super().on_save_checkpoint(trainer, pl_module, checkpoint)
-        val_iou = trainer.callback_metrics.get("val_iou", "Metric not found")
-        val_loss = trainer.callback_metrics.get("val_loss", "Metric not found")
-        print("Epoch:", trainer.current_epoch)
-        print(f"Checkpoint saved with val_iou: {val_iou}")
-        print(f"Checkpoint saved with val_loss: {val_loss}")
-        print("-" * 50)
-        
-    def on_train_end(self, trainer, pl_module):
-        super().on_train_end(trainer, pl_module)
-        if trainer.interrupted:
-            print("*" * 50)
-            print("Training was interrupted by a callback")
-            print("*" * 50)
-        else:
-            print("*" * 50)
-            print("Training Ended Successfully")
-            print("*" * 50)
-
-
-checkpoint_callback = CustomModelCheckpoint(
-    monitor="val_iou",
-    dirpath="checkpoints/",
-    filename=f"lightning_{args.job_id}",
-    save_top_k=1,
-    mode="max",
-)
-
-early_stopping = EarlyStopping(
-    monitor="val_iou",
-    min_delta=0.00,
-    verbose=True,
-    patience=50,
-    mode="max",
-)
 
 if __name__ == "__main__":
     model = LitUnet(
